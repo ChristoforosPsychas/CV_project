bff9f7b378f59ffabb9ead31f199bc7d88a643c2
Christoforos Psychas <37118969+ChristoforosPsychas@users.noreply.github.com>
2026-02-24 13:35:46 +0200

    Add Summary to the main component

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import Header from "./components/Header";
 import WorkExperience from "./components/WorkExperience";
 import Education from "./components/Education";
 import Skills from "./components/Skills";
+import Summary from "./components/Summary";
 
 const App = () => {
   return (
     <div className="max-w-4xl mx-auto my-8 border border-gray-300 shadow-md p-6">
       <Header />
       <main className="mt-4">
+        <Summary />
         <Skills />
         <WorkExperience />
         <Education />
