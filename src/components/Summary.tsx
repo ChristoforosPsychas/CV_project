c78b3eca85f2651f28c54c449adeb6b38710a907
Christoforos Psychas <37118969+ChristoforosPsychas@users.noreply.github.com>
2026-02-24 13:32:10 +0200

    Create Summary component

---
+++ src/components/Summary.tsx
@@ -0,0 +1,21 @@
+const Summary = () => {
+  return (
+    <section className="p-2">
+      <h2 className="text-xl font-bold border-b-2 border-blue-700 mb-2">
+        Summary
+      </h2>
+      <p className="text-sm">
+        After my Master's in Electrical and Computer Engineering, I spent a few
+        years as a professional proofreader while I planned my next move. I
+        eventually realized I missed technical problem-solving, so I spent my
+        evenings teaching myself React and TypeScript to get back into
+        engineering. Now, I build dashboards for EU-funded cybersecurity
+        research projects. It turns out that the same eye for detail I used to
+        catch typos in manuscripts is exactly what I need to build clean,
+        functional user interfaces.
+      </p>
+    </section>
+  );
+};
+
+export default Summary;
